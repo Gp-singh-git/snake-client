d892a7b7af082344ecdfe0a9db1001bc57e1f7d6
gurpreet singh <gurpreet212004@gmail.com>
2021-07-28 18:26:10 +0000

    file initiated and connection established

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:'135.23.223.133' ,
+    port:50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log("Server's data is: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
