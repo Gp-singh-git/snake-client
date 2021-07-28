2390ed59ede0842db2275ac04f6f99aaf226824e
gurpreet singh <gurpreet212004@gmail.com>
2021-07-28 19:44:39 +0000

    connect messages added

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host:'135.23.223.133' ,
+    port:50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log("Server's data is: ", data);
+  });
+
+  conn.on('connect', ()=> console.log("You have been connected to the Game server now !"))
+
+  conn.on('connect', ()=> conn.write("Name:GUR"))
+  return conn;
+};
+
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const net = require("net");
+const connect = require('./client.js');
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:'135.23.223.133' ,
-    port:50542 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (data) => {
-    console.log("Server's data is: ", data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
